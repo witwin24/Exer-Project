4d70073bb37e6050256281b1d34af9ee7baebea2
yaimakmak56 <witthawin.b@kkumail.com>
2025-11-11 17:29:18 +0700

    ex6

---
+++ ex6/app/components/insert-todo.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function InsertTodo(){
+    return <div>
+        <form action="">
+            <input type="text" name="detail" placeholder="detail"/>
+        </form>
+    </div>
+}
